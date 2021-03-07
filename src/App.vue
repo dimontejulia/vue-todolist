42fe03ca950270630bd2253e015a546c584551fc
dimontejulia <dimontejulia@gmail.com>
2021-03-07 15:21:08 -0500

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
+    <Header />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
 
 export default {
   name: "App",
-  components: { Todos },
+  components: { Header, Todos },
   data() {
     return {
       todos: [
