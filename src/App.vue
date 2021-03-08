85d2209d1cb1467152000d9cc225f7320f6f47d5
dimontejulia <dimontejulia@gmail.com>
2021-03-07 19:50:14 -0500

    adding todo form submit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,10 +9,11 @@
 <script>
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: "App",
-  components: { Header, Todos },
+  components: { Header, Todos, AddTodo },
   data() {
     return {
       todos: [
@@ -37,6 +39,9 @@ export default {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id != id);
     },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
   },
 };
 </script>
@@ -51,5 +56,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
