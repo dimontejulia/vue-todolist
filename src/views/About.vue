85fc57fa7bc9f4cd2c176fbe7097b691aff4dcc5
dimontejulia <dimontejulia@gmail.com>
2021-03-07 20:09:19 -0500

    use vue router

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList app v1.0.0. Built following the Vue crash course on
+      Youtube tutorial.
+    </p>
+    <a
+      href="https://www.youtube.com/watch?v=Wy9q22isx3U&ab_channel=TraversyMedia"
+      >Link to course</a
+    >
+  </div>
+</template>
